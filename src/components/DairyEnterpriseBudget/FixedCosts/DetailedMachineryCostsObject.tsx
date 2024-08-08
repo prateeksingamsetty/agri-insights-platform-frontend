1aab123096347d085cb0f7f4552525e638361aec
Prateek Kumar Singamsetty <pksingam@ncsu.edu>
2024-08-08 01:51:25 -0400

    Fixed Costs front end

---
+++ src/components/DairyEnterpriseBudget/FixedCosts/DetailedMachineryCostsObject.tsx
@@ -0,0 +1,639 @@
+export const myDetailedMachineryFixedCosts = [
+  // Articulated Loaders
+  {
+    name: 'articulatedLoadersInitialInvestmentPerUnit',
+    label: 'Articulated Loaders Initial Investment Per Unit'
+  },
+  {
+    name: 'numberOfArticulatedLoaders',
+    label: 'Number of Articulated Loaders'
+  },
+  {
+    name: 'articulatedLoadersTotalHoursOfUse',
+    label: 'Articulated Loaders Total Hours of Use'
+  },
+  {
+    name: 'articulatedLoadersDairyHoursOfUse',
+    label: 'Articulated Loaders Dairy Hours of Use'
+  },
+  {
+    name: 'articulatedLoadersEquipmentAge',
+    label: 'Articulated Loaders Equipment Age'
+  },
+  {
+    name: 'articulatedLoadersYearsOfUsefulLife',
+    label: 'Articulated Loaders Years of Useful Life'
+  },
+
+  // Skid Steer Loaders
+  {
+    name: 'skidSteerLoadersInitialInvestmentPerUnit',
+    label: 'Skid Steer Loaders Initial Investment Per Unit'
+  },
+  { name: 'numberOfSkidSteerLoaders', label: 'Number of Skid Steer Loaders' },
+  {
+    name: 'skidSteerLoadersTotalHoursOfUse',
+    label: 'Skid Steer Loaders Total Hours of Use'
+  },
+  {
+    name: 'skidSteerLoadersDairyHoursOfUse',
+    label: 'Skid Steer Loaders Dairy Hours of Use'
+  },
+  {
+    name: 'skidSteerLoadersEquipmentAge',
+    label: 'Skid Steer Loaders Equipment Age'
+  },
+  {
+    name: 'skidSteerLoadersYearsOfUsefulLife',
+    label: 'Skid Steer Loaders Years of Useful Life'
+  },
+
+  // HP Tractor 130 MFW-D
+  {
+    name: 'hpTractor130MfwDInitialInvestmentPerUnit',
+    label: 'HP Tractor 130 MFW-D Initial Investment Per Unit'
+  },
+  {
+    name: 'numberOfHpTractorMFWD130',
+    label: 'Number of HP Tractor MFWD 130'
+  },
+  {
+    name: 'hpTractor130MfwDTotalHoursOfUse',
+    label: 'HP Tractor 130 MFW-D Total Hours of Use'
+  },
+  {
+    name: 'hpTractor130MfwDDairyHoursOfUse',
+    label: 'HP Tractor 130 MFW-D Dairy Hours of Use'
+  },
+  {
+    name: 'hpTractor130MfwDEquipmentAge',
+    label: 'HP Tractor 130 MFW-D Equipment Age'
+  },
+  {
+    name: 'hpTractor130MfwDYearsOfUsefulLife',
+    label: 'HP Tractor 130 MFW-D Years of Useful Life'
+  },
+
+  // HP Tractor 75
+  {
+    name: 'hpTractor75InitialInvestmentPerUnit',
+    label: 'HP Tractor 75 Initial Investment Per Unit'
+  },
+  { name: 'numberOfHpTractor75', label: 'Number of HP Tractor 75' },
+  {
+    name: 'hpTractor75TotalHoursOfUse',
+    label: 'HP Tractor 75 Total Hours of Use'
+  },
+  {
+    name: 'hpTractor75DairyHoursOfUse',
+    label: 'HP Tractor 75 Dairy Hours of Use'
+  },
+  { name: 'hpTractor75EquipmentAge', label: 'HP Tractor 75 Equipment Age' },
+  {
+    name: 'hpTractor75YearsOfUsefulLife',
+    label: 'HP Tractor 75 Years of Useful Life'
+  },
+
+  // HP Tractor 50
+  {
+    name: 'hpTractor50InitialInvestmentPerUnit',
+    label: 'HP Tractor 50 Initial Investment Per Unit'
+  },
+  { name: 'numberOfHpTractor50', label: 'Number of HP Tractor 50' },
+  {
+    name: 'hpTractor50TotalHoursOfUse',
+    label: 'HP Tractor 50 Total Hours of Use'
+  },
+  {
+    name: 'hpTractor50DairyHoursOfUse',
+    label: 'HP Tractor 50 Dairy Hours of Use'
+  },
+  { name: 'hpTractor50EquipmentAge', label: 'HP Tractor 50 Equipment Age' },
+  {
+    name: 'hpTractor50YearsOfUsefulLife',
+    label: 'HP Tractor 50 Years of Useful Life'
+  },
+
+  // Mixer Wagon 650
+  {
+    name: 'mixerWagon650InitialInvestmentPerUnit',
+    label: 'Mixer Wagon 650 Initial Investment Per Unit'
+  },
+  { name: 'numberOfMixerWagon650', label: 'Number of Mixer Wagon 650' },
+  {
+    name: 'mixerWagon650TotalHoursOfUse',
+    label: 'Mixer Wagon 650 Total Hours of Use'
+  },
+  {
+    name: 'mixerWagon650DairyHoursOfUse',
+    label: 'Mixer Wagon 650 Dairy Hours of Use'
+  },
+  {
+    name: 'mixerWagon650EquipmentAge',
+    label: 'Mixer Wagon 650 Equipment Age'
+  },
+  {
+    name: 'mixerWagon650YearsOfUsefulLife',
+    label: 'Mixer Wagon 650 Years of Useful Life'
+  },
+
+  // Three Quarter Ton Pickup
+  {
+    name: 'threeQuarterTonPickupInitialInvestmentPerUnit',
+    label: 'Three Quarter Ton Pickup Initial Investment Per Unit'
+  },
+  {
+    name: 'numberOfThreeQuarterTonPickup',
+    label: 'Number of Three Quarter Ton Pickup'
+  },
+  {
+    name: 'threeQuarterTonPickupTotalHoursOfUse',
+    label: 'Three Quarter Ton Pickup Total Hours of Use'
+  },
+  {
+    name: 'threeQuarterTonPickupDairyHoursOfUse',
+    label: 'Three Quarter Ton Pickup Dairy Hours of Use'
+  },
+  {
+    name: 'threeQuarterTonPickupEquipmentAge',
+    label: 'Three Quarter Ton Pickup Equipment Age'
+  },
+  {
+    name: 'threeQuarterTonPickupYearsOfUsefulLife',
+    label: 'Three Quarter Ton Pickup Years of Useful Life'
+  },
+
+  // Half Ton Pickup
+  {
+    name: 'halfTonPickupInitialInvestmentPerUnit',
+    label: 'Half Ton Pickup Initial Investment Per Unit'
+  },
+  { name: 'numberOfHalfTonPickup', label: 'Number of Half Ton Pickup' },
+  {
+    name: 'halfTonPickupTotalHoursOfUse',
+    label: 'Half Ton Pickup Total Hours of Use'
+  },
+  {
+    name: 'halfTonPickupDairyHoursOfUse',
+    label: 'Half Ton Pickup Dairy Hours of Use'
+  },
+  {
+    name: 'halfTonPickupEquipmentAge',
+    label: 'Half Ton Pickup Equipment Age'
+  },
+  {
+    name: 'halfTonPickupYearsOfUsefulLife',
+    label: 'Half Ton Pickup Years of Useful Life'
+  },
+
+  // JD Gator
+  {
+    name: 'jdGatorInitialInvestmentPerUnit',
+    label: 'JD Gator Initial Investment Per Unit'
+  },
+  { name: 'numberOfJdGator', label: 'Number of JD Gator' },
+  { name: 'jdGatorTotalHoursOfUse', label: 'JD Gator Total Hours of Use' },
+  { name: 'jdGatorDairyHoursOfUse', label: 'JD Gator Dairy Hours of Use' },
+  { name: 'jdGatorEquipmentAge', label: 'JD Gator Equipment Age' },
+  {
+    name: 'jdGatorYearsOfUsefulLife',
+    label: 'JD Gator Years of Useful Life'
+  },
+
+  //Sand Spreader
+  {
+    name: 'sandSpreaderInitialInvestmentPerUnit',
+    label: 'Sand Spreader Initial Investment Per Unit'
+  },
+  { name: 'numberOfSandSpreader', label: 'Number of Sand Spreader' },
+  {
+    name: 'sandSpreaderTotalHoursOfUse',
+    label: 'Sand Spreader Total Hours of Use'
+  },
+  {
+    name: 'sandSpreaderDairyHoursOfUse',
+    label: 'Sand Spreader Dairy Hours of Use'
+  },
+  { name: 'sandSpreaderEquipmentAge', label: 'Sand Spreader Equipment Age' },
+  {
+    name: 'sandSpreaderYearsOfUsefulLife',
+    label: 'Sand Spreader Years of Useful Life'
+  },
+
+  //hp Tractor 300
+  {
+    name: 'hpTractor300MfwDInitialInvestmentPerUnit',
+    label: 'HP Tractor 300 MFW-D Initial Investment Per Unit'
+  },
+  {
+    name: 'numberOfHpTractorMfwd300',
+    label: 'Number of HP Tractor MFWD 300'
+  },
+  {
+    name: 'hpTractor300MfwDTotalHoursOfUse',
+    label: 'HP Tractor 300 MFW-D Total Hours of Use'
+  },
+  {
+    name: 'hpTractor300MfwDDairyHoursOfUse',
+    label: 'HP Tractor 300 MFW-D Dairy Hours of Use'
+  },
+  {
+    name: 'hpTractor300MfwDEquipmentAge',
+    label: 'HP Tractor 300 MFW-D Equipment Age'
+  },
+  {
+    name: 'hpTractor300MfwDYearsOfUsefulLife',
+    label: 'HP Tractor 300 MFW-D Years of Useful Life'
+  },
+
+  //hp Tractor 200
+  {
+    name: 'hpTractor200MfwDInitialInvestmentPerUnit',
+    label: 'HP Tractor 200 MFW-D Initial Investment Per Unit'
+  },
+  {
+    name: 'numberOfHpTractorMFWD200',
+    label: 'Number of HP Tractor MFWD 200'
+  },
+  {
+    name: 'hpTractor200MfwDTotalHoursOfUse',
+    label: 'HP Tractor 200 MFW-D Total Hours of Use'
+  },
+  {
+    name: 'hpTractor200MfwDDairyHoursOfUse',
+    label: 'HP Tractor 200 MFW-D Dairy Hours of Use'
+  },
+  {
+    name: 'hpTractor200MfwDEquipmentAge',
+    label: 'HP Tractor 200 MFW-D Equipment Age'
+  },
+  {
+    name: 'hpTractor200MfwDYearsOfUsefulLife',
+    label: 'HP Tractor 200 MFW-D Years of Useful Life'
+  },
+
+  //Disk Harrow 24
+  {
+    name: 'diskHarrow24InitialInvestmentPerUnit',
+    label: 'Disk Harrow 24 Initial Investment Per Unit'
+  },
+  { name: 'numberOfDiskHarrow24', label: 'Number of Disk Harrow 24' },
+  {
+    name: 'diskHarrow24TotalHoursOfUse',
+    label: 'Disk Harrow 24 Total Hours of Use'
+  },
+  {
+    name: 'diskHarrow24DairyHoursOfUse',
+    label: 'Disk Harrow 24 Dairy Hours of Use'
+  },
+  { name: 'diskHarrow24EquipmentAge', label: 'Disk Harrow 24 Equipment Age' },
+  {
+    name: 'diskHarrow24YearsOfUsefulLife',
+    label: 'Disk Harrow 24 Years of Useful Life'
+  },
+
+  // Strip-Till Planter 8 Row
+  {
+    name: 'stripTillPlanter8RowInitialInvestmentPerUnit',
+    label: 'Strip-Till Planter 8 Row Initial Investment Per Unit'
+  },
+  {
+    name: 'numberOfStripTillPlanter8Row',
+    label: 'Number of Strip-Till Planter 8 Row'
+  },
+  {
+    name: 'stripTillPlanter8RowTotalHoursOfUse',
+    label: 'Strip-Till Planter 8 Row Total Hours of Use'
+  },
+  {
+    name: 'stripTillPlanter8RowDairyHoursOfUse',
+    label: 'Strip-Till Planter 8 Row Dairy Hours of Use'
+  },
+  {
+    name: 'stripTillPlanter8RowEquipmentAge',
+    label: 'Strip-Till Planter 8 Row Equipment Age'
+  },
+  {
+    name: 'stripTillPlanter8RowYearsOfUsefulLife',
+    label: 'Strip-Till Planter 8 Row Years of Useful Life'
+  },
+
+  // Folding Sprayer 40
+  {
+    name: 'foldingSprayer40InitialInvestmentPerUnit',
+    label: 'Folding Sprayer 40 Initial Investment Per Unit'
+  },
+  { name: 'numberOfFoldingSprayer40', label: 'Number of Folding Sprayer 40' },
+  {
+    name: 'foldingSprayer40TotalHoursOfUse',
+    label: 'Folding Sprayer 40 Total Hours of Use'
+  },
+  {
+    name: 'foldingSprayer40DairyHoursOfUse',
+    label: 'Folding Sprayer 40 Dairy Hours of Use'
+  },
+  {
+    name: 'foldingSprayer40EquipmentAge',
+    label: 'Folding Sprayer 40 Equipment Age'
+  },
+  {
+    name: 'foldingSprayer40YearsOfUsefulLife',
+    label: 'Folding Sprayer 40 Years of Useful Life'
+  },
+
+  // Field Cultivator
+  {
+    name: 'fieldCultivatorInitialInvestmentPerUnit',
+    label: 'Field Cultivator Initial Investment Per Unit'
+  },
+  { name: 'numberOfFieldCultivator', label: 'Number of Field Cultivator' },
+  {
+    name: 'fieldCultivatorTotalHoursOfUse',
+    label: 'Field Cultivator Total Hours of Use'
+  },
+  {
+    name: 'fieldCultivatorDairyHoursOfUse',
+    label: 'Field Cultivator Dairy Hours of Use'
+  },
+  {
+    name: 'fieldCultivatorEquipmentAge',
+    label: 'Field Cultivator Equipment Age'
+  },
+  {
+    name: 'fieldCultivatorYearsOfUsefulLife',
+    label: 'Field Cultivator Years of Useful Life'
+  },
+
+  // Grain Drill - 15’ No-Till
+  {
+    name: 'grainDrill15NoTillInitialInvestmentPerUnit',
+    label: 'Grain Drill 15’ No-Till Initial Investment Per Unit'
+  },
+  {
+    name: 'numberOfGrainDrill15NoTill',
+    label: 'Number of Grain Drill 15’ No-Till'
+  },
+  {
+    name: 'grainDrill15NoTillTotalHoursOfUse',
+    label: 'Grain Drill 15’ No-Till Total Hours of Use'
+  },
+  {
+    name: 'grainDrill15NoTillDairyHoursOfUse',
+    label: 'Grain Drill 15’ No-Till Dairy Hours of Use'
+  },
+  {
+    name: 'grainDrill15NoTillEquipmentAge',
+    label: 'Grain Drill 15’ No-Till Equipment Age'
+  },
+  {
+    name: 'grainDrill15NoTillYearsOfUsefulLife',
+    label: 'Grain Drill 15’ No-Till Years of Useful Life'
+  },
+
+  // Mower Conditioner (Self-Propelled)
+  {
+    name: 'mowerConditionerSelfPropelledInitialInvestmentPerUnit',
+    label: 'Mower Conditioner (Self-Propelled) Initial Investment Per Unit'
+  },
+  {
+    name: 'numberOfMowerConditionerSelfPropelled',
+    label: 'Number of Mower Conditioner (Self-Propelled)'
+  },
+  {
+    name: 'mowerConditionerSelfPropelledTotalHoursOfUse',
+    label: 'Mower Conditioner (Self-Propelled) Total Hours of Use'
+  },
+  {
+    name: 'mowerConditionerSelfPropelledDairyHoursOfUse',
+    label: 'Mower Conditioner (Self-Propelled) Dairy Hours of Use'
+  },
+  {
+    name: 'mowerConditionerSelfPropelledEquipmentAge',
+    label: 'Mower Conditioner (Self-Propelled) Equipment Age'
+  },
+  {
+    name: 'mowerConditionerSelfPropelledYearsOfUsefulLife',
+    label: 'Mower Conditioner (Self-Propelled) Years of Useful Life'
+  },
+
+  // Tedder
+  {
+    name: 'tedderInitialInvestmentPerUnit',
+    label: 'Tedder Initial Investment Per Unit'
+  },
+  { name: 'numberOfTedder', label: 'Number of Tedder' },
+  { name: 'tedderTotalHoursOfUse', label: 'Tedder Total Hours of Use' },
+  { name: 'tedderDairyHoursOfUse', label: 'Tedder Dairy Hours of Use' },
+  { name: 'tedderEquipmentAge', label: 'Tedder Equipment Age' },
+  { name: 'tedderYearsOfUsefulLife', label: 'Tedder Years of Useful Life' },
+
+  // Power Rake
+  {
+    name: 'powerRakeInitialInvestmentPerUnit',
+    label: 'Power Rake Initial Investment Per Unit'
+  },
+  { name: 'numberOfPowerRake', label: 'Number of Power Rake' },
+  {
+    name: 'powerRakeTotalHoursOfUse',
+    label: 'Power Rake Total Hours of Use'
+  },
+  {
+    name: 'powerRakeDairyHoursOfUse',
+    label: 'Power Rake Dairy Hours of Use'
+  },
+  { name: 'powerRakeEquipmentAge', label: 'Power Rake Equipment Age' },
+  {
+    name: 'powerRakeYearsOfUsefulLife',
+    label: 'Power Rake Years of Useful Life'
+  },
+
+  // 15’ Folding Rotary Mower
+  {
+    name: 'foldingRotaryMower15InitialInvestmentPerUnit',
+    label: '15’ Folding Rotary Mower Initial Investment Per Unit'
+  },
+  {
+    name: 'numberOfFoldingRotaryMower15',
+    label: 'Number of 15’ Folding Rotary Mower'
+  },
+  {
+    name: 'foldingRotaryMower15TotalHoursOfUse',
+    label: '15’ Folding Rotary Mower Total Hours of Use'
+  },
+  {
+    name: 'foldingRotaryMower15DairyHoursOfUse',
+    label: '15’ Folding Rotary Mower Dairy Hours of Use'
+  },
+  {
+    name: 'foldingRotaryMower15EquipmentAge',
+    label: '15’ Folding Rotary Mower Equipment Age'
+  },
+  {
+    name: 'foldingRotaryMower15YearsOfUsefulLife',
+    label: '15’ Folding Rotary Mower Years of Useful Life'
+  },
+
+  // Deep-Ripper
+  {
+    name: 'deepRipperInitialInvestmentPerUnit',
+    label: 'Deep-Ripper Initial Investment Per Unit'
+  },
+  { name: 'numberOfDeepRipper', label: 'Number of Deep-Ripper' },
+  {
+    name: 'deepRipperTotalHoursOfUse',
+    label: 'Deep-Ripper Total Hours of Use'
+  },
+  {
+    name: 'deepRipperDairyHoursOfUse',
+    label: 'Deep-Ripper Dairy Hours of Use'
+  },
+  { name: 'deepRipperEquipmentAge', label: 'Deep-Ripper Equipment Age' },
+  {
+    name: 'deepRipperYearsOfUsefulLife',
+    label: 'Deep-Ripper Years of Useful Life'
+  },
+
+  // 24’ Livestock Trailer
+  {
+    name: 'livestockTrailer24InitialInvestmentPerUnit',
+    label: '24’ Livestock Trailer Initial Investment Per Unit'
+  },
+  {
+    name: 'numberOfLivestockTrailer24',
+    label: 'Number of 24’ Livestock Trailer'
+  },
+  {
+    name: 'livestockTrailer24TotalHoursOfUse',
+    label: '24’ Livestock Trailer Total Hours of Use'
+  },
+  {
+    name: 'livestockTrailer24DairyHoursOfUse',
+    label: '24’ Livestock Trailer Dairy Hours of Use'
+  },
+  {
+    name: 'livestockTrailer24EquipmentAge',
+    label: '24’ Livestock Trailer Equipment Age'
+  },
+  {
+    name: 'livestockTrailer24YearsOfUsefulLife',
+    label: '24’ Livestock Trailer Years of Useful Life'
+  },
+
+  // Round Baler
+  {
+    name: 'roundBalerInitialInvestmentPerUnit',
+    label: 'Round Baler Initial Investment Per Unit'
+  },
+  { name: 'numberOfRoundBaler', label: 'Number of Round Baler' },
+  {
+    name: 'roundBalerTotalHoursOfUse',
+    label: 'Round Baler Total Hours of Use'
+  },
+  {
+    name: 'roundBalerDairyHoursOfUse',
+    label: 'Round Baler Dairy Hours of Use'
+  },
+  { name: 'roundBalerEquipmentAge', label: 'Round Baler Equipment Age' },
+  {
+    name: 'roundBalerYearsOfUsefulLife',
+    label: 'Round Baler Years of Useful Life'
+  },
+
+  // Tub Grinder
+  {
+    name: 'tubGrinderInitialInvestmentPerUnit',
+    label: 'Tub Grinder Initial Investment Per Unit'
+  },
+  { name: 'numberOfTubGrinder', label: 'Number of Tub Grinder' },
+  {
+    name: 'tubGrinderTotalHoursOfUse',
+    label: 'Tub Grinder Total Hours of Use'
+  },
+  {
+    name: 'tubGrinderDairyHoursOfUse',
+    label: 'Tub Grinder Dairy Hours of Use'
+  },
+  { name: 'tubGrinderEquipmentAge', label: 'Tub Grinder Equipment Age' },
+  {
+    name: 'tubGrinderYearsOfUsefulLife',
+    label: 'Tub Grinder Years of Useful Life'
+  },
+
+  // Miscellaneous Equipment
+  {
+    name: 'miscellaneousEquipmentInitialInvestmentPerUnit',
+    label: 'Miscellaneous Equipment Initial Investment Per Unit'
+  },
+  {
+    name: 'numberOfMiscellaneousEquipment',
+    label: 'Number of Miscellaneous Equipment'
+  },
+  {
+    name: 'miscellaneousEquipmentTotalHoursOfUse',
+    label: 'Miscellaneous Equipment Total Hours of Use'
+  },
+  {
+    name: 'miscellaneousEquipmentDairyHoursOfUse',
+    label: 'Miscellaneous Equipment Dairy Hours of Use'
+  },
+  {
+    name: 'miscellaneousEquipmentEquipmentAge',
+    label: 'Miscellaneous Equipment Equipment Age'
+  },
+  {
+    name: 'miscellaneousEquipmentYearsOfUsefulLife',
+    label: 'Miscellaneous Equipment Years of Useful Life'
+  },
+
+  // Other Machinery Equipment 1
+  {
+    name: 'otherMachineryEquipment1InitialInvestmentPerUnit',
+    label: 'Other Machinery Equipment 1 Initial Investment Per Unit'
+  },
+  {
+    name: 'numberOfOtherMachineryEquipment1',
+    label: 'Number of Other Machinery Equipment 1'
+  },
+  {
+    name: 'otherMachineryEquipment1TotalHoursOfUse',
+    label: 'Other Machinery Equipment 1 Total Hours of Use'
+  },
+  {
+    name: 'otherMachineryEquipment1DairyHoursOfUse',
+    label: 'Other Machinery Equipment 1 Dairy Hours of Use'
+  },
+  {
+    name: 'otherMachineryEquipment1EquipmentAge',
+    label: 'Other Machinery Equipment 1 Equipment Age'
+  },
+  {
+    name: 'otherMachineryEquipment1YearsOfUsefulLife',
+    label: 'Other Machinery Equipment 1 Years of Useful Life'
+  },
+
+  // Other Machinery Equipment 2
+  {
+    name: 'otherMachineryEquipment2InitialInvestmentPerUnit',
+    label: 'Other Machinery Equipment 2 Initial Investment Per Unit'
+  },
+  {
+    name: 'numberOfOtherMachineryEquipment2',
+    label: 'Number of Other Machinery Equipment 2'
+  },
+  {
+    name: 'otherMachineryEquipment2TotalHoursOfUse',
+    label: 'Other Machinery Equipment 2 Total Hours of Use'
+  },
+  {
+    name: 'otherMachineryEquipment2DairyHoursOfUse',
+    label: 'Other Machinery Equipment 2 Dairy Hours of Use'
+  },
+  {
+    name: 'otherMachineryEquipment2EquipmentAge',
+    label: 'Other Machinery Equipment 2 Equipment Age'
+  },
+  {
+    name: 'otherMachineryEquipment2YearsOfUsefulLife',
+    label: 'Other Machinery Equipment 2 Years of Useful Life'
+  }
+]
