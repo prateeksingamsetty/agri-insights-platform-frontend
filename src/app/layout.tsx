6749c5732deeb6e3ea3c2be3fece1fb3e57d2527
Prateek Kumar Singamsetty <pksingam@ncsu.edu>
2024-08-12 16:21:32 -0400

    minor sidebar height bug fixed

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -17,7 +17,7 @@ export default function RootLayout({
   return (
     <AuthProvider>
       <html lang='en'>
-        <body className={`${inter.className} flex min-h-screen flex-col`}>
+        <body className={`${inter.className} flex min-h-full flex-col`}>
           <UtilityBar />
           <Navbar />
           {/* <div className='flex flex-1 pt-[108px]'>
@@ -25,7 +25,7 @@ export default function RootLayout({
             <main className='ml-64 flex-1 p-4'>{children}</main>
           </div> */}
           <div className='mt-20 flex h-full flex-1 p-2'>
-            <div className='sticky top-0 h-screen w-64 overflow-y-auto bg-white'>
+            <div className='sticky top-0 h-full w-64 overflow-y-auto bg-white'>
               <Sidebar />
             </div>
             <main className='flex-1 overflow-y-auto p-4'>{children}</main>
