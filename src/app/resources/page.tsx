ea806c3db010525ac7556c7b39022f74d4180d9a
Prateek Kumar Singamsetty <pksingam@ncsu.edu>
2025-02-18 17:29:45 -0500

    deleted unecessary files

--- src/app/resources/page.tsx
+++ src/app/resources/page.tsx
@@ -1,10 +1,7 @@
-import UnderConstructionImage from '@components/UnderConstructionImage'
-
 const ResourcesPage = () => {
   return (
     <>
       <h1 className='text-center text-xl text-blue-600'>Resources</h1>
-      <UnderConstructionImage />
     </>
   )
 }
--- src/app/tomato/page.tsx
+++ src/app/tomato/page.tsx
@@ -1,10 +1,7 @@
-import UnderConstructionImage from '@components/UnderConstructionImage'
-
 const TomatoLandingPage = () => {
   return (
     <>
       <h1 className='text-center text-xl text-blue-600'>Tomato Landing Page</h1>
-      <UnderConstructionImage />
     </>
   )
 }
